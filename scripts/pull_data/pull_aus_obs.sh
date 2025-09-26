26551cdfa69691d7b71eb4f67388c453a541fe8a
Ubuntu <ubuntu@ip-172-31-31-193.ap-southeast-2.compute.internal>
2025-09-26 01:29:48 +0000

    Updated permissions

--- scripts/pull_data/pull_aus_obs.sh
+++ scripts/pull_data/pull_aus_obs.sh
@@ -21,6 +21,7 @@ trap 'rm -f "$tmpfile"' EXIT
 # Create the lftp script
 {
  echo "set sftp:auto-confirm yes"
+ echo "set xfer:clobber yes"
  echo "open -u anonymous,ryan.mcnie@linz.govt.nz sftp://sftp.data.gnss.ga.gov.au"
  echo "lcd ${localDir}/tmp"
  echo "cd /rinex/highrate/$PrevYEAR/$PrevDOY/23/"
@@ -32,16 +33,20 @@ trap 'rm -f "$tmpfile"' EXIT
  echo "bye"
 } > "$tmpfile"
 
-# Execute and cleanup (trap handles cleanup even on error)
-lftp -f "$tmpfile"
+count=0
+lftp -f "$tmpfile" 2>&1 | while read -r line; do
+  ((count++))
+  echo -ne "\rDownloading file #$count: ${line:0:80}"
+done
+echo
 
 echo
-total=$(ls -1 $localDir/*.gz 2>/dev/null | wc -l)
+total=$(ls -1 ${TMPDIR}/*.gz 2>/dev/null | wc -l)
 count=0
 
 
 
-for file in *.gz; do
+for file in ${TMPDIR}/*.gz; do
     gzip -d -f "$file"
     ((count++))
     percent=$((count * 100 / total))
@@ -50,24 +55,14 @@ for file in *.gz; do
 done
 echo
 
-total=$(ls -1 *.crx 2>/dev/null | wc -l)
+total=$(ls -1 ${TMPDIR}/*.crx 2>/dev/null | wc -l)
 count=0
 
-for file in *.crx; do
-    crx2rnx -d -f "$file"
+for file in ${TMPDIR}/*.crx; do
+    ./tools/crx2rnx -d -f "$file"
     ((count++))
     percent=$((count * 100 / total))
     bar=$(printf "%-${percent}s" "#" | tr ' ' '#')
     printf "\rConverting .crx files: [%-100s] %d%%" "$bar" "$percent"
 done
 
-# Loop through all files in the directory to capitalise
-for file in *.rnx; do
-    # Check if it's a file (not a directory)
-    if [ -f "$file" ]; then
-        # Get the uppercase version of the filename
-        uppercase_file=$(echo "$file" | tr '[:lower:]' '[:upper:]')
-        # Rename the file
-        mv "$file" "$uppercase_file"
-    fi
-done
